7cbfcb4c5c5e13bac3ae24e7b26b412ea8d41d69
Jianyu Li <jianyucode@gmail.com>
2019-09-22 23:23:16 -0600

    react random number game

---
+++ src/Random.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class Random extends Component {
+  state = { num: 1,};
+
+  randomNumber = (e) => {
+    let randNum = Math.floor(Math.random()*10)+1;
+
+    this.setState({num : randNum})
+  }
+
+  render(){
+    // let randNum = Math.floor(Math.random()*10)+1;
+    return (
+      <div>
+        <h1>Number is {this.state.num}</h1>
+        {this.state.num === 7 && <h2>You WIN!!!</h2>}
+        {this.state.num !==7 && <button onClick={this.randomNumber}>Click me for random number</button>}
+      </div>
+    )
+  }
+}
+
+export default Random;
